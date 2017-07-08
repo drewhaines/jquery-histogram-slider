e3f4c841747c5efe3ed0a63b8899e6a520a3352e
bgriswold <bgriswold@fairwaytech.com>
2017-07-07 18:12:09 -0700

    General clean up for naming mostly and private functions. Initial setup to support optimal range which doesn't start at zero.

--- histogram.slider.js
+++ histogram.slider.js
@@ -1,39 +1,35 @@
-// TODO
-// Integrate with knockout.js
-// External .css
-
-// https://jqueryboilerplate.com/
-// https://github.com/jquery-boilerplate/jquery-boilerplate/wiki/jQuery-boilerplate-and-demo
 ; (function ($, window, document, undefined) {
-
     var pluginName = "histogramSlider",
         dataKey = "plugin_" + pluginName;
 
-    var updateHistogram = function (leftHandleValue, rightHandleValue, isLeftSlider, sliderMin, rangePerBin, histogramName, sliderName) {
-        $( "#" + sliderName + "-value" ).html( leftHandleValue + " - " + rightHandleValue );
+    var updateHistogram = function (selectedRange, isLeftSlider, sliderMin, rangePerBin, histogramName, sliderName) {
+        var leftValue = selectedRange[0],
+            rightValue = selectedRange[1];
+
+        $("#" + sliderName + "-value").html(leftValue + " - " + rightValue);
 
         // set opacity per bin based on the slider values
         $("#"+ histogramName + " .in-range").each(function (index, bin) {
             maxBinValue = rangePerBin * (index + 1);
             minBinValue = (rangePerBin * index) + sliderMin;
 
-            if (isLeftSlider && maxBinValue < rightHandleValue) {
+            if (isLeftSlider && maxBinValue < rightValue) {
                 // Set opacity based on left (min) slider
-                if (leftHandleValue > maxBinValue) {
+                if (leftValue > maxBinValue) {
                     setOpacity(bin, 0);
-                } else if (leftHandleValue < minBinValue) {
+                } else if (leftValue < minBinValue) {
                     setOpacity(bin, 1);
                 } else {
-                    setOpacity(bin, 1 - (leftHandleValue - minBinValue) / rangePerBin);
+                    setOpacity(bin, 1 - (leftValue - minBinValue) / rangePerBin);
                 }
-            } else if (!isLeftSlider && minBinValue > leftHandleValue) {
+            } else if (!isLeftSlider && minBinValue > leftValue) {
                 // Set opacity based on right (max) slider value
-                if (rightHandleValue > maxBinValue) {
+                if (rightValue > maxBinValue) {
                     setOpacity(bin, 1);
-                } else if (rightHandleValue < minBinValue) {
+                } else if (rightValue < minBinValue) {
                     setOpacity(bin, 0);
                 } else {
-                    setOpacity(bin, (rightHandleValue - minBinValue) / rangePerBin);
+                    setOpacity(bin, (rightValue - minBinValue) / rangePerBin);
                 }
             }
         });
@@ -43,20 +39,34 @@
         $(bin).css("opacity", val);
     };
 
+    var calculateHeightRatio = function(bins, height) {
+        var heightRatio = 1, maxValue = 0;
+
+        for (i = 0; i < bins.length; i++) {
+            if (bins[i] > maxValue) {
+                maxValue = bins[i];
+            }
+        }
+
+        if (parseInt(5 * maxValue + 1) > height) {
+            heightRatio = (height) / (5 * maxValue + 1);
+        }
+
+        return heightRatio;
+    }
+
     var Plugin = function (element, options) {
         this.element = element;
 
         this.options = {
-            sliderMin: 0,
-            sliderMax: 1000000,
-            leftHandleValue: 0,
-            rightHandleValue: 1000000,
-            backgroundColorInRange: "#0079BA",
-            backgroundColorOutOfRange: "#DBE0E2",
-            optimalBackgroundColorInRange: "#01E2D4",
-            optimalBackgroundColorOutOfRange: "#caf9f6",
+            sliderRange: [0, 1000000], // Min and Max slider values
+            optimalRange: [0, 0], // Optimal range to select within
+            selectedRange: [0, 0], // Min and Max slider values selected
+            color: "#DBE0E2",
+            colorSelected: "#0079BA",
+            colorOptimal: "#CAF9F6",
+            colorOptimalSelected: "#01E2D4",
             height: 200,
-            optimalRange: 0,
             numberOfBins: 40,
             showTooltips: false,
             showValues: true
@@ -70,42 +80,34 @@
             $.extend(this.options, options);
 
             var self = this,
-                histoData = self.options.data,
+                dataItems = self.options.data.items,
                 bins = new Array(this.options.numberOfBins).fill(0),
-                widthPerBin = 100 / this.options.numberOfBins,
-                range = self.options.sliderMax - self.options.sliderMin,
-                rangePerBin = range / this.options.numberOfBins,
-                maxValue = 0,
-                heightRatio = 1,
-                histogramName = self.element.attr('id') + "-histogram",
-                sliderName = self.element.attr('id') + "-slider";
+                range = self.options.sliderRange[1] - self.options.sliderRange[0],
+                rangePerBin = range / this.options.numberOfBins;;
 
             // iterate through data and increment the correct bin based on items.value
-            for (i = 0; i < histoData.items.length; i++) {
-                var index = parseInt(histoData.items[i].value / rangePerBin),
+            for (i = 0; i < dataItems.length; i++) {
+                var index = parseInt(dataItems[i].value / rangePerBin),
                     increment = 1;
 
-                if (histoData.items[i].count) {
-                    increment = parseInt(histoData.items[i].count);
+                if (dataItems[i].count) {
+                    // Handle grouped data structure
+                    increment = parseInt(dataItems[i].count);
                 }
 
                 bins[index] += increment;
             }
 
-            for (i = 0; i < bins.length; i++) {
-                if (bins[i] > maxValue) {
-                    maxValue = bins[i];
-                }
-            }
+            var histogramName = self.element.attr('id') + "-histogram",
+                sliderName = self.element.attr('id') + "-slider";
 
-            if (parseInt(5 * maxValue + 1) > self.options.height) {
-                heightRatio =  (self.options.height)/(5 * maxValue+1);
-            }
-
-            var wrapHtml = "<div id='" + histogramName + "' style='height:" + self.options.height + "px; overflow: hidden;'></div>" +
+            var wrapHtml = "<div id='" + histogramName + "' style='height:" + self.options.height + "px; overflow: hidden;border: 1px solid black;'></div>" +
                 "<div id='" + sliderName + "'></div>";
 
             self.element.html(wrapHtml);
+
+            var heightRatio = calculateHeightRatio(bins, self.options.height),
+                widthPerBin = 100 / this.options.numberOfBins;
 
             // create histogram based on bins. 5px of height for every item in that bin.
             for (i = 0; i < bins.length; i++) {
@@ -113,16 +115,16 @@
                     h = parseInt(5 * rh + 1),               // set the actual height using the relative height
                     b = parseInt(self.options.height - h),  // set the bottom offset for the in-range bin
                     bb = -parseInt(self.options.height - h * 2),  // set the bottom offset for the out-of-range bin
-                    minBinValue = (rangePerBin * i) + this.options.sliderMin,
+                    minBinValue = (rangePerBin * i) + this.options.sliderRange[0],
                     maxBinValue = rangePerBin * (i + 1) - 1,
-                    inRangeStyling = ((self.options.optimalRange > minBinValue) ? "background-color: " + this.options.optimalBackgroundColorInRange + ";" : "background-color: " + this.options.backgroundColorInRange + ";"),
-                    outRangeStyling = ((self.options.optimalRange > minBinValue) ? "background-color: " + this.options.optimalBackgroundColorOutOfRange + ";" : "background-color: " + this.options.backgroundColorOutOfRange + ";"),
+                    inRangeStyling = ((self.options.optimalRange[1] > minBinValue) ? "background-color: " + this.options.colorOptimalSelected + ";" : "background-color: " + this.options.colorSelected + ";"),
+                    outRangeStyling = ((self.options.optimalRange[1] > minBinValue) ? "background-color: " + this.options.colorOptimal + ";" : "background-color: " + this.options.color + ";"),
                     showTooltip = (self.options.showTooltips ? "" : "display-none")
 
                 var binHtml = "<div class='tooltip' style='float:left!important;width:" + widthPerBin + "%;'>" +
                     "<span class='tooltiptext " + showTooltip + "'>" + minBinValue + " - " + maxBinValue + ", count: " + bins[i] +"</span>" +
-                    "<div class='bin in-range' style='z-index:1;height:" + h + "px;bottom:-" + b + "px;" + inRangeStyling + "'></div>" +
-                    "<div class='bin out-of-range' style='z-index:0;height:" + h + "px;bottom:" + bb + "px;" + outRangeStyling + "'></div>" +
+                    "<div class='bin in-range' style='z-index:1;height:" + h + "px;bottom:-" + b + "px;" + inRangeStyling + ";position: relative;'></div>" +
+                    "<div class='bin out-of-range' style='z-index:0;height:" + h + "px;bottom:" + bb + "px;" + outRangeStyling + "position: relative;'></div>" +
                     "</div>";
 
                 $("#" + histogramName).append(binHtml);
@@ -130,11 +132,11 @@
 
             $("#" + sliderName).slider({
                 range: true,
-                min: self.options.sliderMin,
-                max: self.options.sliderMax,
-                values: [self.options.leftHandleValue, self.options.rightHandleValue],
+                min: self.options.sliderRange[0],
+                max: self.options.sliderRange[1],
+                values: self.options.selectedRange,
                 slide: function (event, ui) {
-                    updateHistogram(ui.values[0], ui.values[1], Boolean(ui.handle.nextSibling), self.options.sliderMin, rangePerBin, histogramName, sliderName);
+                    updateHistogram(ui.values, Boolean(ui.handle.nextSibling), self.options.sliderRange[0], rangePerBin, histogramName, sliderName);
                 }
             });
 
@@ -142,8 +144,8 @@
               $("#" + sliderName).after("<p id='" + sliderName + "-value' style='text-align: center;'></p>");
             }
 
-            updateHistogram(self.options.leftHandleValue, self.options.rightHandleValue, false, self.options.sliderMin, rangePerBin, histogramName, sliderName);
-            updateHistogram(self.options.leftHandleValue, self.options.rightHandleValue, true, self.options.sliderMin, rangePerBin, histogramName, sliderName);
+            updateHistogram(self.options.selectedRange, false, self.options.sliderRange[0], rangePerBin, histogramName, sliderName);
+            updateHistogram(self.options.selectedRange, true, self.options.sliderRange[0], rangePerBin, histogramName, sliderName);
         }
     };
 
